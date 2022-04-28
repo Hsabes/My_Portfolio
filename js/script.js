153357e448b5a8ee9cbe661f41e0adb83e380e38
Harrison Sabean <hsabes@gmail.com>
2022-04-28 14:39:45 -0700

    added script to aboutme.html that makes div and text appear on top of image when button is pressed

--- js/script.js
+++ js/script.js
@@ -3,13 +3,3 @@
 // me page, create boxes that are the exact same dimensions of the cursor, call
 // them triggers. If the location of the cursor === the location of the trigger,
 // activate the transition of the panel, displaying the various texts in each panel
-
-const currentOpacity = document.querySelector('#aboutMeImageText');
-        
-function changeOpacity() {
-    if (currentOpacity.style.opacity === 0) {
-        currentOpacity.stlye.opacty = 1;
-    } else {
-        currentOpacity.style.opacity = 0;
-    }
-}
