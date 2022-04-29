e98794a2ee2b2a2cd46dd2e96ac92c3a2a23937e
Harrison Sabean <hsabes@gmail.com>
2022-04-29 14:36:05 -0700

    modified aboutme.html and aboutme.css so the panels now transition in. The text content of the buttons that activate each panel now have an altered text content depending on the state of the panel they have activated or deactivated

--- js/script.js
+++
@@ -1,5 +0,0 @@
-// Within About Me page: create a program that moves a box around the screen within
-// a container, call the box "cursor" maybe. Underneath each panel on the about
-// me page, create boxes that are the exact same dimensions of the cursor, call
-// them triggers. If the location of the cursor === the location of the trigger,
-// activate the transition of the panel, displaying the various texts in each panel
